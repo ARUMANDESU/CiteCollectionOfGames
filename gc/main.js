70fc8dcb22deb9154425d8502e3491b31fbf709b
mauk14 <92082502+mauk14@users.noreply.github.com>
2021-10-31 14:59:56 +0600

    Update main.js

--- gc/main.js
+++ gc/main.js
@@ -1,3 +1,10 @@
 function goGames() {
     document.location.href = 'games/games.html';
 }
+function goGame(x, y) {
+    document.location.href = 'games' + x + '/game' + y + '.html';
+}
+
+function goGameFromIntel(y) {
+    document.location.href = 'games/games1/game' + y + '.html';
+}
